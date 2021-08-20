002d87a073f32503d1df4eef8c85c83f13e21c86
khadakkar1997 <82271766+khadakkar1997@users.noreply.github.com>
2021-08-20 08:52:17 +0530

    Update LineCompersion.java

--- LineCompersion.java
+++ LineCompersion.java
@@ -1,41 +1,30 @@
 package LineComapre;
 
+import java.util.*;
+
 public class LineCompersion {
 
-	private int x1;
-	private int y1;
-	private int x2;
-	private int y2;
+	// Create a method to find the length of line through taking coordinates from user's
+	public Double myMethod() {
 
-	public LineCompersion(int x1, int y1, int x2, int y2) {
-		this.x1 = x1;
-		this.y1 = y1;
-		this.x2 = x2;
-		this.y2 = y2;
-	}
-
-	static void compareTo(int length1, int length2) {
-		if (length1 == length2)
-			System.out.println("length 1 is equal to length 2");
-		else if (length1 > length2)
-			System.out.println("length 1 is greater than length 2");
-		else
-			System.out.println("length 2 is greater than length 1");
-	}
-
-	static double calculate_length(LineCompersion obj) {
-		double length = Math.sqrt(Math.pow(obj.x2 - obj.x1, 2) + Math.pow(obj.y2 - obj.y1, 2));
-		return length;
+		Scanner myObj = new Scanner(System.in);
+		System.out.println(" Welcome to Line Compersion Computation ");
+		System.out.println("Enter x1, y1, x2, y2:  ");
+		int coordinate_x1 = myObj.nextInt();
+		int coordinate_y1 = myObj.nextInt();
+		int coordinate_x2 = myObj.nextInt();
+		int coordinate_y2 = myObj.nextInt();
+		System.out.println("enter coordinate_x1: " + coordinate_x1);
+		System.out.println("enter coordinate_y1: " + coordinate_y1);
+		System.out.println("enter coordinate_x2: " + coordinate_x2);
+		System.out.println("enter coordinate_y2: " + coordinate_y2);
+		int Length = (int) (Math.sqrt((coordinate_x2 - coordinate_x1) ^ 2 + (coordinate_y2 - coordinate_y1) ^ 2));
+		System.out.println("Length of a line: " + Length);
+		return (double) Length;
 	}
 
 	public static void main(String[] args) {
-		LineCompersion line1 = new LineCompersion(5, 3, 3, 4);
-		LineCompersion line2 = new LineCompersion(8, 3, 3, 4);
-		int length1 = (int) calculate_length(line1);
-		int length2 = (int) calculate_length(line2);
-		System.out.println("length 1: " + length1);
-		System.out.println("length 2: " + length2);
-		compareTo(length1, length2);
+		LineCompersion sc = new LineCompersion();
+		sc.myMethod();
 	}
-
 }
